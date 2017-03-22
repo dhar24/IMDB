d1eb6cf65c050de0407a6d0dbcc00d5e11b069cd
Dharmendra Agarwal <omaandk@gmail.com>
2017-03-22 12:14:28 -0700

    no message

--- Imdb_scraping.R
+++ Imdb_scraping.R
@@ -2,14 +2,7 @@ library(rvest)
 url<- read_html("http://www.imdb.com/title/tt2771200/")
 title<- url %>% html_nodes(".title_wrapper h1") %>% html_text()
 rating<-  url %>% html_nodes(".ratingValue strong span") %>% html_text()%>% as.numeric()
-num_users<-  url %>% html_nodes(".ratings_wrapper a span") %>% html_text()%>% as.numeric()
+num_users<-  url %>% html_nodes(".ratings_wrapper a span") %>% html_text()%>%
 type<-  url %>% html_nodes(" .subtext .itemprop") %>% html_text()
-type<- 
-
+type<- paste(type,collapse = " ")
 data<-data.frame(title,rating,num_users,type)
-
-
-
-
-
-
