e56a5ebccde1934287847f56b247251109dd3def
Dharmendra Agarwal <omaandk@gmail.com>
2017-03-26 23:56:19 -0700

    no message

--- Imdb_scraping.R
+++ Imdb_scraping.R
@@ -1,8 +1,22 @@
 library(rvest)
-url<- read_html("http://www.imdb.com/title/tt2771200/")
-title<- url %>% html_nodes(".title_wrapper h1") %>% html_text()
-rating<-  url %>% html_nodes(".ratingValue strong span") %>% html_text()%>% as.numeric()
-num_users<-  url %>% html_nodes(".ratings_wrapper a span") %>% html_text()%>%
-type<-  url %>% html_nodes(" .subtext .itemprop") %>% html_text()
+df<-data.frame()
+titles<-0
+scrape<-function(urlss)
+{
+url1<- read_html(urlss)
+titles<- url1 %>% html_nodes(".title_wrapper h1") %>% html_text()
+print(title)
+rating<-  url1 %>% html_nodes(".ratingValue strong span") %>% html_text()%>% as.numeric()
+num_users<-  url1 %>% html_nodes(".ratings_wrapper a span") %>% html_text()
+type<-  url1 %>% html_nodes(" .subtext .itemprop") %>% html_text()
 type<- paste(type,collapse = " ")
-data<-data.frame(title,rating,num_users,type)
+df1<-data.frame(title,rating,num_users,type)
+df<-rbind(df,df1)
+}
+urls<-0
+base_url<-"http://www.imdb.com/title/"
+for(c in 1:2){
+  urls[c]<- paste(base_url,"tt",c,sep="")
+  scrape(urls[c])
+  
+}
